ff7a5d0d0f4c8632190b4b0fef73116ab75ba1e1
Aleksey Nelipa <aleksey.nelipa@gmail.com>
2026-01-09 02:07:18 -0500

    Added settings screen - empty for now

--- android-app/app/src/main/java/com/example/kanjiquiz/ui/QuizActivity.kt
+++ android-app/app/src/main/java/com/example/kanjiquiz/ui/QuizActivity.kt
@@ -4,211 +4,97 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
 import com.example.kanjiquiz.app.App
-import com.example.kanjiquiz.data.VocabEntry
 import com.example.kanjiquiz.ui.theme.KanjiQuizTheme
 
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModels {
-        val app = application as App
-        QuizViewModelFactory(app.vocabRepository)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KanjiQuizTheme {
-                MainScreen(viewModel)
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun MainScreen(viewModel: MainViewModel) {
-    val stateData by viewModel.stateData.collectAsState()
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+fun SettingsScreen(modifier: Modifier) {
+    ScreenCard(modifier) {
+        Text("TODO: Settings Screen", fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("KanjiQuiz") },
+                actions = {
+                    IconButton(onClick = {
+                        val currentRoute = navController.currentBackStackEntry?.destination?.route
+                        if (currentRoute != "Settings")
+                            navController.navigate("Settings")
+                        else
+                            navController.popBackStack()
+                    }) {
+                        Icon(Icons.Default.Settings, contentDescription = "Settings")
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
         val modifier = Modifier.padding(innerPadding)
-        when (stateData.quizPhase) {
-            QuizPhase.Question -> Question(
-                viewModel = viewModel,
-                modifier = modifier
-            )
+        NavHost(
+            navController,
+            navController.createGraph("Quiz") {
+                composable("Quiz") { backStackEntry ->
+                    val context = LocalContext.current
+                    val app = context.applicationContext as App
 
-            QuizPhase.CorrectAnswer -> Answer(
-                viewModel = viewModel,
-                isAnswerCorrect = true,
-                modifier = modifier
-            )
+                    val viewModel: MainViewModel = viewModel(
+                        viewModelStoreOwner = backStackEntry,
+                        factory = QuizViewModelFactory(app.vocabRepository)
+                    )
 
-            QuizPhase.IncorrectAnswer -> Answer(
-                viewModel = viewModel,
-                isAnswerCorrect = false,
-                modifier = modifier
-            )
-
-            QuizPhase.Loading -> Loading(modifier = modifier)
-            QuizPhase.Empty -> Empty(modifier = modifier)
-        }
+                    QuizScreen(viewModel, modifier)
+                }
+                composable("Settings") { backStackEntry ->
+                    SettingsScreen(modifier)
+                }
+            })
     }
 }
 
-@Composable
-private fun Loading(modifier: Modifier) {
-    ScreenCard(modifier) {
-        Text("Loading", fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
-    }
-}
-
-@Composable
-private fun Empty(modifier: Modifier) {
-    ScreenCard(modifier) {
-        Text("Empty", fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
-    }
-}
-
-
-@Composable
-private fun Question(viewModel: MainViewModel, modifier: Modifier = Modifier) {
-    val stateData by viewModel.stateData.collectAsState()
-    val currentEntry = stateData.currentEntry ?: return
-
-    val focusRequester = remember {FocusRequester()}
-    ScreenCard(modifier) {
-        Text(currentEntry.expression, fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
-
-        Spacer(modifier = Modifier.height(12.dp))
-        TextField(
-            value = stateData.answer,
-            singleLine = true,
-            onValueChange = viewModel::setAnswer,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
-            keyboardActions = KeyboardActions(onSend = { viewModel.submit() }),
-            label = { Text("Enter text", fontSize = 20.sp) },
-            textStyle = LocalTextStyle.current.copy(fontSize = 25.sp),
-            modifier = Modifier.fillMaxWidth().focusRequester(focusRequester)
-        )
-
-        if(stateData.validationMessage!="") {
-            Text(stateData.validationMessage,
-                fontSize = 20.sp, color = Color.Red, modifier = Modifier.fillMaxWidth())
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Button(onClick = { viewModel.submit() }, Modifier
-            .fillMaxWidth()
-            .size(64.dp)) {
-            Text("Submit", fontSize = 25.sp)
-        }
-
-        val keyboardController = LocalSoftwareKeyboardController.current
-        LaunchedEffect(Unit) {
-            focusRequester.requestFocus()
-            keyboardController?.show()
-        }
-
-    }
-}
-
-@Composable
-private fun Answer(
-    viewModel: MainViewModel,
-    isAnswerCorrect: Boolean,
-    modifier: Modifier = Modifier
-) {
-    val stateData by viewModel.stateData.collectAsState()
-    val currentEntry = stateData.currentEntry ?: return
-    ScreenCard(modifier) {
-
-        Text(currentEntry.expression, fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        Text(
-            if (isAnswerCorrect) "Correct" else "Wrong",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            color = if (isAnswerCorrect) Color.Green else Color.Red
-        )
-
-        Text(
-            "Your answer: ${stateData.answer}",
-            fontSize = 25.sp,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            "Correct: ${currentEntry.reading}",
-            fontSize = 25.sp,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            "Meaning: ${currentEntry.meaning}",
-            fontSize = 25.sp,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Button(onClick = { viewModel.next() }, Modifier
-            .fillMaxWidth()
-            .size(64.dp)) {
-            Text("Next", fontSize = 25.sp)
-        }
-    }
-
-}
-
-@Composable
-private fun ScreenCard(
-    modifier: Modifier = Modifier,
-    content: @Composable androidx.compose.foundation.layout.ColumnScope.() -> Unit
-) {
-    Card(modifier = modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            content()
-        }
-    }
-}
-
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
@@ -227,3 +113,5 @@ fun Preview() {
         MainScreen(viewModel)
     }
 }
+
+ */
---
+++ android-app/app/src/main/java/com/example/kanjiquiz/ui/QuizScreen.kt
@@ -0,0 +1,160 @@
+package com.example.kanjiquiz.ui
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun QuizScreen(viewModel: MainViewModel, modifier: Modifier) {
+    val stateData by viewModel.stateData.collectAsState()
+    when (stateData.quizPhase) {
+        QuizPhase.Question -> Question(
+            viewModel = viewModel,
+            modifier = modifier
+        )
+
+        QuizPhase.CorrectAnswer -> Answer(
+            viewModel = viewModel,
+            isAnswerCorrect = true,
+            modifier = modifier
+        )
+
+        QuizPhase.IncorrectAnswer -> Answer(
+            viewModel = viewModel,
+            isAnswerCorrect = false,
+            modifier = modifier
+        )
+
+        QuizPhase.Loading -> Loading(modifier = modifier)
+        QuizPhase.Empty -> Empty(modifier = modifier)
+    }
+
+}
+@Composable
+private fun Loading(modifier: Modifier) {
+    ScreenCard(modifier) {
+        Text("Loading", fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+private fun Empty(modifier: Modifier) {
+    ScreenCard(modifier) {
+        Text("Empty", fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+
+@Composable
+private fun Question(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+    val stateData by viewModel.stateData.collectAsState()
+    val currentEntry = stateData.currentEntry ?: return
+
+    val focusRequester = remember {FocusRequester()}
+    ScreenCard(modifier) {
+        Text(currentEntry.expression, fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(12.dp))
+        TextField(
+            value = stateData.answer,
+            singleLine = true,
+            onValueChange = viewModel::setAnswer,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
+            keyboardActions = KeyboardActions(onSend = { viewModel.submit() }),
+            label = { Text("Enter text", fontSize = 20.sp) },
+            textStyle = LocalTextStyle.current.copy(fontSize = 25.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester)
+        )
+
+        if(stateData.validationMessage!="") {
+            Text(stateData.validationMessage,
+                fontSize = 20.sp, color = Color.Red, modifier = Modifier.fillMaxWidth())
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(onClick = { viewModel.submit() }, Modifier
+            .fillMaxWidth()
+            .size(64.dp)) {
+            Text("Submit", fontSize = 25.sp)
+        }
+
+        val keyboardController = LocalSoftwareKeyboardController.current
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+            keyboardController?.show()
+        }
+
+    }
+}
+
+@Composable
+private fun Answer(
+    viewModel: MainViewModel,
+    isAnswerCorrect: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val stateData by viewModel.stateData.collectAsState()
+    val currentEntry = stateData.currentEntry ?: return
+    ScreenCard(modifier) {
+
+        Text(currentEntry.expression, fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            if (isAnswerCorrect) "Correct" else "Wrong",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = if (isAnswerCorrect) Color.Green else Color.Red
+        )
+
+        Text(
+            "Your answer: ${stateData.answer}",
+            fontSize = 25.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            "Correct: ${currentEntry.reading}",
+            fontSize = 25.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            "Meaning: ${currentEntry.meaning}",
+            fontSize = 25.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(onClick = { viewModel.next() }, Modifier
+            .fillMaxWidth()
+            .size(64.dp)) {
+            Text("Next", fontSize = 25.sp)
+        }
+    }
+
+}
